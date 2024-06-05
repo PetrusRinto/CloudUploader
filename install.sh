e6287ed6f98bf18ace91b45aed462cbbe00e854e
PetrusRinto <114573430+PetrusRinto@users.noreply.github.com>
2024-06-05 15:54:02 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+# install.sh
+
+# You need to use sudo priveileges to install this 
+
+if [ "$(id -u)" -ne 0 ]; then
+    echo "This script must be run by root. Please try sudo first"
+    exit 1
+fi
+
+# Adding the files to the user's $PATH
+
+cp clouduploader.sh /usr/local/bin/clouduploader
+chmod +x /usr/local/bin/clouduploader
+mv /usr/local/bin/clouduploader/clouduploader.sh clouduploader
+
+echo "Clouduploader has been installed to /usr/local/bin/clouduploader"
